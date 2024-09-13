3e9e0c0b539d65f9aa1ffe434521075a02621c69
hjsmkj <144957873+hjsmkj@users.noreply.github.com>
2024-09-13 20:06:22 +0300

    Update function_test.cpp

--- function_test.cpp
+++ function_test.cpp
@@ -2,17 +2,17 @@
 #include "function.hpp"
 
 TEST(FunctionTests, PositiveNums) {
-    EXPECT_EQ(function(1, 2), 4); // 1 + 2 * (1 + 1) = 4
-    EXPECT_EQ(function(2, 3), 8); // 2 + 3 * (2 + 1) = 11
+    EXPECT_EQ(function(1, 2), 5); // 1 + 2 * (1 + 1) = 5
+    EXPECT_EQ(function(2, 3), 11); // 2 + 3 * (2 + 1) = 11
 }
 
 TEST(FunctionTests, NegativeAndZeroNums) {
     EXPECT_EQ(function(0, 0), 0); // 0 + 0 * (0 + 1) = 0
-    EXPECT_EQ(function(-1, 1), 0); // -1 + 1 * (-1 + 1) = -1
-    EXPECT_EQ(function(-2, -2), -8); // -2 + (-2) * (-2 + 1) = -2
+    EXPECT_EQ(function(-1, 1), -1); // -1 + 1 * (-1 + 1) = -1
+    EXPECT_EQ(function(-2, -2), 0); // -2 + (-2) * (-2 + 1) = 0
 }
 
 TEST(FunctionTests, Scobki) {
-    EXPECT_EQ(function(100, 0), 10000); // 100 + 0 * (100 + 1) = 100
-    EXPECT_EQ(function(0, 100), 0); // 0 + 100 * (0 + 1) = 100
+    EXPECT_EQ(function(100, 0), 100); // 100 + 0 * (100 + 1) = 100
+    EXPECT_EQ(function(0, 100), 100); // 0 + 100 * (0 + 1) = 100
 }
